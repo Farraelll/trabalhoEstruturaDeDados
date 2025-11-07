2ca437e08516d71f5371b87d34d34a3aadada92a
João Vitor <91470763+JoaoVitorOliveiraScheidegger@users.noreply.github.com>
2025-11-07 20:46:56 -0300

    Update bdtimes.h

--- bdtimes.h
+++ bdtimes.h
@@ -11,28 +11,12 @@ typedef struct {
 } BDTimes;
 
 // Prototipos das Funcoes
-
-
 BDTimes* bdtimes_criar();
-
-// Libera a memoria alocada para o BDTimes
 void bdtimes_liberar(BDTimes* bdt);
-
-// Carrega os times do arquivo times.csv
 int bdtimes_carregar(BDTimes* bdt, const char* arquivo_csv);
-
-// Processa uma partida, atualizando as estatisticas dos dois times envolvidos
 void bdtimes_processar_partida(BDTimes* bdt, int id_time1, int gols_time1, int id_time2, int gols_time2);
-
-// Retorna um ponteiro para um time, dado seu ID
-// Retorna NULL se nao encontrado
-
 Time* bdtimes_buscar_por_id(BDTimes* bdt, int id);
-
-// Retorna um ponteiro para um time, usando o indice
 Time* bdtimes_buscar_por_indice(BDTimes* bdt, int index);
-
-// Retorna o numero de times
 int bdtimes_get_num_times(BDTimes* bdt);
 
 #endif
