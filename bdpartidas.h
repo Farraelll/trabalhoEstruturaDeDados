f06a1758ae241e12f4a687fed5e0adceba54c89f
João Vitor <91470763+JoaoVitorOliveiraScheidegger@users.noreply.github.com>
2025-11-07 20:51:54 -0300

    Update bdpartidas.h

--- bdpartidas.h
+++ bdpartidas.h
@@ -14,19 +14,9 @@ typedef struct {
 
 // Prototipos das Funcoes
 BDPartidas* bdpartidas_criar();
-
-// Libera a memoria alocada para o BDPartidas
 void bdpartidas_liberar(BDPartidas* bdp);
-
-// Carrega as partidas do arquivo partidas.csv 
-// Retorna 0 em sucesso, 1 em falha.
-
 int bdpartidas_carregar(BDPartidas* bdp, const char* arquivo_csv);
-
-// Retorna um ponteiro para uma partida, usando o indice
 Partida* bdpartidas_buscar_por_indice(BDPartidas* bdp, int index);
-
-// Retorna o numero total de partidas carregadas
 int bdpartidas_get_num_partidas(BDPartidas* bdp);
 
 #endif
