4b799b543ad44be69fad020ea764587bd1b40e17
João Vitor <91470763+JoaoVitorOliveiraScheidegger@users.noreply.github.com>
2025-11-07 20:47:36 -0300

    Update time.h

--- time.h
+++ time.h
@@ -14,20 +14,10 @@ typedef struct {
 } Time;
 
 // Prototipos das Funcoes
-
-// Declara time_inicializar
 void time_inicializar(Time* time, int id, const char* nome);
-
-// Declara time_processar_partida
 void time_processar_partida(Time* time, int gols_feitos, int gols_sofridos);
-
-// Declara time_get_pg
 int time_get_pg(Time* time);
-
-// Declara time_get_s
 int time_get_s(Time* time);
-
-// Declara time_imprimir_estatisticas
 void time_imprimir_estatisticas(Time* time);
 
 #endif
